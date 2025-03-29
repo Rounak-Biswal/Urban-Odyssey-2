6b08eb14fa712e6318a3298709d1897096e432e6
Rounak_Biswal <rounakbiswal2003@gmail.com>
2025-03-30 01:24:52 +0530

    star rating added

--- router/userRoutes.js
+++ router/userRoutes.js
@@ -5,20 +5,22 @@ const { saveRedirectUrl } = require("../utils/middleware")
 const passport = require("passport");
 const { signup, renderLoginForm, renderSignupForm, logout, login } = require("../controller/userController");
 
-router.get("/signup", renderSignupForm)
+router
+    .route("/signup")
+    .get(renderSignupForm)
+    .post(wrapAsync(signup))
 
-router.post("/signup", wrapAsync(signup))
-
-router.get("/login", renderLoginForm)
-
-router.post("/login",
-    saveRedirectUrl,
-    passport.authenticate("local", {
-        failureRedirect: "/users/login",
-        failureFlash: true
-    }),
-    login
-)
+router
+    .route("/login")
+    .get(renderLoginForm)
+    .post(
+        saveRedirectUrl,
+        passport.authenticate("local", {
+            failureRedirect: "/users/login",
+            failureFlash: true
+        }),
+        login
+    )
 
 router.get("/logout", logout);
 
